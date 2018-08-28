151823ac9dc830c1549618f91f7a71a383ea6dfd
Jaaren Sacharow <jsacharow@HQ-MC-JSACHAROW.local>
2018-08-28 15:48:58 -0400

    fixed and complete, reducer was acting up

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -5,55 +5,26 @@ const initialState = {
   items: [],
   item: {}
 };
-
-export default function(state = initialState, action) {
-  switch (action.type) {
-    case FETCH_POSTS:
-    console.log(`====>>>>>>>>>>>>>>>>>>>>>>.${action.playload}`)
-      return {
-        ...state,
-        items: action.payload
-      };
-    case NEW_POST:
-    console.log(`====<<<<<<<<,.${action.playload}`)
-      return {
-        ...state,
-        item: action.payload
-      };
-    default:
-      return state;
-  }
-}
-
-
-// import { FETCH_POSTS, NEW_POST } from '../actions/types';
-
-// //list
-// //single post
-// const initialState = {
-//     items: [], 
-//     item: {}
-// }
-
 // // the reducer accepts actions dispatched from the action files,
 // //takes a copy of the current state, and then creates a new state
 // // based on the payload sent from the action.
 
-// export default function(state = initialState, action) {
-//     switch(action.type) {
-//         case FETCH_POSTS:
-//             return {
-//                 ...state,
-//                 items: action.payload
-//             };
-//         case NEW_POST:
-//             return {
-//                 ...state,
-//                 item: action.playload
-//             };
 
-//         default:
-//             return state;
-    
-//     }
-// }
+export default function(state= initialState, action) {
+    switch(action.type) {
+        case FETCH_POSTS:  //check for the action type from the dispatcher
+            return {
+                ...state,  //whole previous state
+                items: action.payload //whole list of items
+            };
+        case NEW_POST:
+            return {
+                ...state,
+                item: action.payload //new item
+            };
+        default:
+            return state;
+
+    }
+}
+
